8589e0c29c0efd77c3f965dfe9459463b2a39d52
Aniket Naik <eraniket26@gmail.com>
2026-08-14 13:00:44 +0530

    Version 2: Added reset endpoint to clear history from in memory database

--- app.py
+++ app.py
@@ -43,12 +43,17 @@ def logVote(name) :
 
   
 #    print(f'Exisitng names in list', existing_names)
-   return jsonify ({'msg': "Your vote has been successfully recorded."})
+   return jsonify ({'msg': "Your vote has been successfully recorded."}, 200)
 
 
 @app.route('/results')
 def displayResults():
     return voting_dictonary
+
+@app.route('/reset')
+def resetVotes() :
+    voting_dictonary.clear()
+    return jsonify({'msg': 'Votes record reset successfully'}, 200)
 
 
 if __name__ == '__main__':
